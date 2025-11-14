f77875c02015e83b677dcf6de86ebd4283c381e6
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-11-14 13:38:36 -0600

    bivariate analysis - edit theme

--- 2_bivariate_analysis.R
+++ 2_bivariate_analysis.R
@@ -28,14 +28,19 @@ netflix |>
     ) +
   scale_color_manual(
     values = c("Movie" = "#FF9F9B", "TV Show" = "#A1C9F4"), # changing colors for lines
-    labels = c("Movie", "TV Show") # adding labels for the legend
+    labels = c("Movie", "TV Show"), # adding labels for the legend
+    name = NULL
   ) +
   labs(
     title = "TBA",
     caption = "TBA",
     x = "Year Added",
-    y = "Counts",
-    color = "Type of Listing"
+    y = "Counts"
   ) +
-  theme_minimal() 
+  theme_classic() +
+  theme(
+    legend.position = c(0.5, 1),
+    legend.justification = c(0.5, 1), # legend positioning
+    legend.direction = "horizontal"
+  )
 
