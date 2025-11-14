3139a3003311f3ec41616d6d3d1833a14c14166b
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-11-14 12:57:37 -0600

    univariate analysis

--- 1_univariate_analysis.R
+++ 1_univariate_analysis.R
@@ -7,11 +7,20 @@ library(tidyverse)
 # read in data ----
 netflix <- read_csv("data/netflix_titles.csv")
 
+# analysis ----
+
 # distinct countries
-netflix |>
+countries <- netflix |>
   separate_rows(country, sep = ",\\s*") |>
   distinct(country) |>
-  filter(!is.na(country) & country != "NA")
+  filter(!is.na(country), country != "NA")
+
+# number of times each country appears in dataset 
+country_counts <- netflix |>
+  separate_rows(country, sep = ",\\s*") |>
+  count(country, sort = TRUE) |>
+  na.omit() 
+
 
 # distinct genres
 netflix |>
@@ -19,6 +28,45 @@ netflix |>
   distinct(listed_in) |>
   filter(!is.na(listed_in) & listed_in != "NA")
 
+# number of times each genre/category appears in dataset 
+genre_counts <- netflix |>
+  separate_rows(listed_in, sep = ",\\s*") |>
+  count(listed_in, sort = TRUE) |>
+  na.omit() 
+
+# plot of genre counts
+genre_counts |>
+  ggplot(aes(listed_in, n)) +
+  geom_col() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 5)
+  )
+
+# date added col
+netflix <- netflix |>
+  mutate(
+    date_added = lubridate::mdy(date_added), # converting col to date type
+    year_added = lubridate::year(date_added) # extracting year added
+    )
+
+year_counts <- netflix |> # counting frequency years appear in dataset
+  count(year_added, sort = TRUE) |>
+  na.omit()
+
+year_counts |> # plotting frequencies
+  ggplot(aes(year_added, n)) +
+  geom_col(fill = "#4E2A84") +
+  scale_x_continuous(
+    breaks = seq(2008, 2021, 1),
+    expand = c(0, 0)
+  ) +
+  labs(
+    title = "TBA",
+    caption = "TBA",
+    x = "Year Added",
+    y = "Counts"
+  ) +
+  theme_minimal() 
 
 ## plotting distribution of  ----
 # netflix |>
@@ -29,7 +77,7 @@ netflix |>
 #     y = "Number of Observations",
 #     x = "Reference Area")
 
-## bar graph that shows the number of observations within each year in the dataset ----
+## bar graph that shows the number of observations within each release year in the dataset ----
 netflix |>
   ggplot(aes(release_year)) +
   geom_bar(fill = "black") +
