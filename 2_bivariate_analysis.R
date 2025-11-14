a30b9b5fd6dfdb3b2b53bf178af7486908db9041
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-11-14 13:23:23 -0600

    bivariate analysis

--- 2_bivariate_analysis.R
+++ 2_bivariate_analysis.R
@@ -1,2 +1,11 @@
 ## STAT 302 Final Project ----
 # Bivariate Analysis ----
+
+
+# type of listing vs year released ----
+netflix |>
+  count(type, release_year) |>
+  ggplot(aes(release_year, n, color = type)) +
+  geom_line()
+
+
