52daf47d4fde3244b244ee4ca4126fec5ffc77bb
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-11-14 13:33:44 -0600

    bivariate analysis - type of listing vs year added

--- 2_bivariate_analysis.R
+++ 2_bivariate_analysis.R
@@ -13,4 +13,25 @@ netflix |>
   ggplot(aes(release_year, n, color = type)) +
   geom_line()
 
+# type of listing vs year added ----
+netflix |>
+  mutate(
+    date_added = lubridate::mdy(date_added), # converting col to date type
+    year_added = lubridate::year(date_added) # extracting year added
+  ) |>
+  count(type, year_added) |>
+  ggplot(aes(year_added, n, color = type)) +
+  geom_line() +
+  scale_x_continuous(
+    breaks = seq(2008, 2021, 1),
+    expand = c(0, 0)
+    ) +
+  labs(
+    title = "TBA",
+    caption = "TBA",
+    x = "Year Added",
+    y = "Counts",
+    color = "Type of Listing"
+  ) +
+  theme_minimal() 
 
