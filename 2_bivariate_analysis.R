909739317b7feac0403ddd106f773b682302493a
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-11-20 09:28:22 -0600

    edit plot title size

--- 2_bivariate_analysis.R
+++ 2_bivariate_analysis.R
@@ -67,4 +67,6 @@ netflix |>
     fill = "Type of Listing"
     ) + 
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        plot.title = element_text(size = 18)
+        )
